e5d9d403c988ee82d7ac411839dae6680b32aa59
Eldenbraz <noxuszero@gmail.com>
2021-05-04 12:22:20 -0400

    added MP handling in dreams

--- commands/songes_command.js
+++ commands/songes_command.js
@@ -12,6 +12,9 @@ module.exports = {
             return message.channel.send('MANQUE LES ARGUMENTS');
         if (args[0] == "help")
             return message.channel.send("Utilisation de la commande \"Songes\": .songes [Nom du monstre] [Niveau]\nLa commande supporte les espaces, mais pas la recherche de plusieurs monstres en même temps. Pour plus d'informations sur un monstre, utiliser la commande Abs.");
+        
+        var level_songe = args[args.length-1]
+        args.pop()
 
         async function test() {
             fetch('https://dofensive.com/api/monsterPreview.php?Language=fr')
@@ -36,32 +39,43 @@ module.exports = {
                                         fetch(`https://dofensive.com/api/spell.php?Id=${x.Id}&Language=fr`)
                                             .then(res => res.json())
                                             .then(async function (json) {
-                                                resolve({ 
+                                                var a, b, c, d;
+                                                var retour,buffer;
+                                                // console.log(json.Levels[0].Effects[0].Actions);
+                                                // console.log("--------------------------------------------------------------------------------------\n");
+                                                retour += "PA : **" + json.Levels[0].ActionPoints + "**\tCrit : **" + json.Levels[0].CriticalProbability + "%**"
+                                                retour += "\nPortée : **"+json.Levels[0].MinRange+" - "+json.Levels[0].Range+"** > Portée **"
+                                                retour += (json.Levels[0].EditableRange == true ? "Modifiable" : "Non Modifiable" )+"**\n"
+                                                retour += (json.Levels[0].CastInLine == true ? "Lancer en ligne Uniquement\n" : "")
+                                                retour += (json.Levels[0].CastInDiagonal == true ? "Lancer en Diagonale Possible\n" : "")
+                                                retour += (json.Levels[0].CastLineOfSight == true ? "Ligne de Vue nécessaire\n" : "Ne nécessite pas de Ligne de Vue\n")
+                                                retour += (json.Levels[0].NeedFreeCell == true ? "Ne nécessite pas de cible\n" : "")
+                                                retour += (json.Levels[0].NeedTakenCell == true ? "Nécessite une cible\n" : "")
+                                                retour += (json.Levels[0].NeedFreeTrapCell == true ? "Nécessite une cellule **libre**\n" : "")
+                                                retour += (json.Levels[0].MaxStack > 0 ? `Cumul max des effets : ${json.Levels[0].MaxStack}\n` : "")
+                                                retour += (json.Levels[0].MaxCastPerTurn > 0 ? `Utilisations par tour : ${json.Levels[0].MaxCastPerTurn}\n` : "")
+                                                retour += (json.Levels[0].MaxCastPerTarget > 0 ? `Utilisations par cible : ${json.Levels[0].MaxCastPerTarget}\n` : "")
+                                                retour += (json.Levels[0].MinCastInterval > 0 ? `Intervale de relance : ${json.Levels[0].MinCastInterval}\n` : "")
+                                                retour += (json.Levels[0].Effects[0].Dispellable > 0 ? "*Peut être désenvouté*\n" : "")
+
+                                                retour += ("\`\`\`apache\n")
+                                                for (a = 0; a < json.Levels[0].Effects.length; a++) {
+                                                    buffer = (json.Levels[0].Effects[a].Description)
+                                                    for (b = 0; b < json.Levels[0].Effects[a].Actions.length; b++) {
+                                                        d = ((json.Levels[0].Effects[a].Description).match(/#/g)).length
+                                                        buffer = (buffer.replace("#"+(b+1),json.Levels[0].Effects[a].Actions[b].Value.Name))
+                                                        for (c = (b+1); d = buffer.match(/#/g) != null; c++) {
+                                                            buffer = (buffer.replace("#"+(b+c),json.Levels[0].Effects[a].Actions[b+1]?.Value.Name))
+                                                            buffer = (buffer.replace("#"+(b+c),json.Levels[0].Effects[a].Actions[b].Value.Name))
+                                                        }
+                                                    }
+                                                    retour += (buffer+"\n")
+                                                }
+                                                retour += (`<Zone d'effet> : ${json.Levels[0].Effects[0].Zone.Value}\n`)
+                                                retour += (`\`\`\`\n`)
+                                                resolve({
                                                     name: x.Name, 
-                                                    value: 
-                                                    "PA : **" + json.Levels[0].ActionPoints + "**\tCrit : **" + json.Levels[0].CriticalProbability + "%**" +
-                                                    "\nPortée : **"+json.Levels[0].MinRange+" - "+json.Levels[0].Range+"** > Portée **"+
-                                                    (json.Levels[0].EditableRange == true ? "Modifiable" : "Non Modifiable" )+"**\n" + 
-                                                    (json.Levels[0].CastInLine == true ? "Lancer en ligne Uniquement\n" : "") +
-                                                    (json.Levels[0].CastInDiagonal == true ? "Lancer en Diagonale Possible\n" : "") +
-                                                    (json.Levels[0].CastLineOfSight == true ? "Ligne de Vue nécessaire\n" : "Ne nécessite pas de Ligne de Vue\n") +
-                                                    (json.Levels[0].NeedFreeCell == true ? "Ne nécessite pas de cible\n" : "") +
-                                                    (json.Levels[0].NeedTakenCell == true ? "Nécessite une cible\n" : "") +
-                                                    (json.Levels[0].NeedFreeTrapCell == true ? "Nécessite une cellule **libre**\n" : "") +
-                                                    (json.Levels[0].MaxStack > 0 ? `Cumul max des effets : ${json.Levels[0].MaxStack}\n` : "") +
-                                                    (json.Levels[0].MaxCastPerTurn > 0 ? `Utilisations par tour : ${json.Levels[0].MaxCastPerTurn}\n` : "") +
-                                                    (json.Levels[0].MaxCastPerTarget > 0 ? `Utilisations par cible : ${json.Levels[0].MaxCastPerTarget}\n` : "") +
-                                                    (json.Levels[0].MinCastInterval > 0 ? `Intervale de relance : ${json.Levels[0].MinCastInterval}\n` : "") +
-                                                    (json.Levels[0].Effects[0].Dispellable > 0 ? "*Peut être désenvouté*\n" : "") +
-                                                    //.replace("#3",`${json.Levels[0].Effects[0].Actions[2]?.Value.Name}`)
-                                                    (json.Levels[0].Effects[0].Actions[1]?.Value.Name ? `\`\`\`apache\n${json.Levels[0].Effects[0].Description.replace("#1",`${json.Levels[0].Effects[0].Actions[0].Value.Name}`).replace("#2",`${json.Levels[0].Effects[0].Actions[1].Value.Name}`)}\n` : (json.Levels[0].Effects[0].Description.replace("#1",`${json.Levels[0].Effects[0].Actions[0]?.Value.Name}`) ? `\`\`\`apache\n${json.Levels[0].Effects[0].Description.replace("#1",`${json.Levels[0].Effects[0].Actions[0]?.Value.Name}`)}\n` : "")) +
-                                                    (`<Zone d'effet> : ${json.Levels[0].Effects[0].Zone.Value}\`\`\`\n`) +
-                                                    (json.Levels[0].Effects[1]?.Actions[1]?.Value.Name ? `\`\`\`apache\n${json.Levels[0].Effects[1].Description.replace("#1",`${json.Levels[0].Effects[1].Actions[0].Value.Name}`).replace("#2",`${json.Levels[0].Effects[1].Actions[1].Value.Name}`)}\n` : (json.Levels[0].Effects[1]?.Description?.replace("#1",`${json.Levels[0].Effects[1].Actions[0]?.Value.Name}`) ? `\`\`\`apache\n${json.Levels[0].Effects[1]?.Description?.replace("#1",`${json.Levels[0].Effects[1].Actions[0]?.Value.Name}`)}\n` : "")) +
-                                                    (json.Levels[0].Effects[1]?.Zone.Value ? `<Zone d'effet> : ${json.Levels[0].Effects[1]?.Zone.Value}\`\`\`\n` : "") +
-                                                    (json.Levels[0].Effects[2]?.Actions[1]?.Value.Name ? `\`\`\`apache\n${json.Levels[0].Effects[2].Description.replace("#1",`${json.Levels[0].Effects[2].Actions[0].Value.Name}`).replace("#2",`${json.Levels[0].Effects[2].Actions[1].Value.Name}`)}\n` : (json.Levels[0].Effects[2]?.Description?.replace("#1",`${json.Levels[0].Effects[2].Actions[0]?.Value.Name}`) ? `\`\`\`apache\n${json.Levels[0].Effects[2]?.Description?.replace("#1",`${json.Levels[0].Effects[2].Actions[0]?.Value.Name}`)}\n` : "")) +
-                                                    (json.Levels[0].Effects[2]?.Zone.Value ? `<Zone d'effet> : ${json.Levels[0].Effects[2]?.Zone.Value}\`\`\`\n` : "") +
-                                                    (json.Levels[0].Effects[3]?.Actions[1]?.Value.Name ? `\`\`\`apache\n${json.Levels[0].Effects[3].Description.replace("#1",`${json.Levels[0].Effects[3].Actions[0].Value.Name}`).replace("#2",`${json.Levels[0].Effects[3].Actions[1].Value.Name}`)}\n` : (json.Levels[0].Effects[3]?.Description?.replace("#1",`${json.Levels[0].Effects[3].Actions[0]?.Value.Name}`) ? `\`\`\`apache\n${json.Levels[0].Effects[3]?.Description?.replace("#1",`${json.Levels[0].Effects[3].Actions[0]?.Value.Name}`)}\n` : "")) +
-                                                    (json.Levels[0].Effects[3]?.Zone.Value ? `<Zone d'effet> : ${json.Levels[0].Effects[3]?.Zone.Value}\`\`\`\n` : "") 
+                                                    value: retour 
                                                 });
                                             })
                                     })
@@ -87,7 +101,7 @@ module.exports = {
                                 .setImage(`https://dofensive.com/data/picture/${test.Id}.webp`)
                                 .addField('HPs',json.Grades[0].LifePoints,true)
                                 .addField('APs',json.Grades[0].ActionPoints,true)
-                                .addField('MPs',json.Grades[0].MovementPoints,true)
+                                .addField('MPs',(json.Grades[0].MovementPoints+Math.floor(Math.pow(((level_songe-json.Level)/70),0.77))),true)
                                 // .setURL(`https://dofensive.com/fr/monster/${test.Id}`)
                             message.channel.send({ embed });
                         });
